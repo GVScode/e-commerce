da36c7c62dfe0e04e7826968187982a19d89fba9
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-02-23 11:12:00 -0300

    incluindo os produtos em db.sql

--- db.sql
+++ db.sql
@@ -18,3 +18,15 @@ CREATE TABLE products (
 INSERT INTO users (email, password)
 VALUES ('admin@email.com', MD5('123456'));
 
+INSERT INTO `products`
+(`name`, `description`, `price`, `image`) 
+VALUES 
+('Controle PS5','Controle Bluetooth','400.00','assets/images/controle-ps5.webp')
+('Cadeira Gamer','Cadeira confortável','800.00','assets/images/cadeira-gamer.jpg')
+('Echo Show','Echo Show Alexa','1200.00','assets/images/echo show 5.webp'),
+('Fire tv stick hd','Fire TV stick hd','299.99','assets/images/firestick.webp'),
+('Fone Bluetooth','Fone Bluetooth','399.00','assets/images/fone_bluetooth.jpg'),
+('Headset','Headset gamer','299.99','assets/images/headset.jpg'),
+('Iphone 17 PRO MAX','Iphone 17 PRO MAX','14.000','assets/images/iphone.jpeg'),
+('Nitendo Switch','Nitendo Switch','2400.00','assets/images/nitendo-switch.jpg'))
+
